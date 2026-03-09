8cfab082f62b2f6aa5aae16e223ae025e54696d8
Akiel Stewart <140494913+AkielStew09@users.noreply.github.com>
2026-03-08 21:12:02 -0500

    Started writing the js
    I've started with the js, closely following the pseudocode I laid out in
    the txt file. So far I've only grabbed the relevant elements. I will
    start writing the function next.

---
+++ script.js
@@ -0,0 +1,16 @@
+//grab all inputs
+const email = document.querySelector("#email");
+const country = document.querySelector("#country");
+const postalCode = document.querySelector("#postalCode");
+const pw = document.querySelector("#pw");
+const pwConfirm = document.querySelector("#pwConfirm");
+
+//grab the form
+const form = document.querySelector("#submissionForm");
+
+//grab the error spans
+const emailError = document.querySelector("#emailError");
+const countryError = document.querySelector("#countryError");
+const postalCodeError = document.querySelector("#postalCodeError");
+const pwError = document.querySelector("#pwError");
+const pwConfirmError = document.querySelector("#pwConfirmError");
