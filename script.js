e0f178980ffd94ceca373d0924d75d94020cb8d0
Akiel Stewart <140494913+AkielStew09@users.noreply.github.com>
2026-03-14 12:27:05 -0500

    Validated all fields except pw confirm
    I've gotten the form fields working. The submit action on the form is working
    as well. All that is left is to make sure the password confirm is the
    same as the password.

--- script.js
+++ script.js
@@ -43,31 +43,36 @@ form.addEventListener("submit", (e) => {
 
 //returns whether all fields are valid
 const allValid = () => {
+    let result = true;
     elements.forEach((element) => {
-        //if any of the elements is invalid, return false
-        if (!element.validity.valid) {
-            return false;
+        //if any of the elements is invalid
+        //or empty, assign false to result
+        alert(`testing ${element.id}`);
+        if (!element.validity.valid || element.value == "") {
+            alert(`${element.id} has a problem`);
+            result = false;
+        } else {
+            alert(`${element.id} has no problems`);
         }
     });
-    //if it reaches this point, then all are valid, so return true
-    return true;
+    //if it reaches this point, then result should be true
+    //return result
+    return result;
 }
 
 //calls showError on every element
 const showAllErrors = () => {
     //for every element
-    for (let i = 0; i < elements.length; ++i) {
-        //call showError and pass each element 
-        //along with its corresponding error
-        showError(elements[i], errors[i]);
-    }
+    elements.forEach((elem) => {
+        showError(elem);
+    })
 };
 
 const showError = (element) => {
     //retrieve the corresponding error since I don't want to have to pass it
     let error = errors[`${element.id}`];
 
-    if (element.value == "") {
+    if (element.value === "") {
         let message = ` You need to fill the ${element.name} field.`;
         element.setCustomValidity(message);
         error.textContent = message;
@@ -76,12 +81,51 @@ const showError = (element) => {
         error.textContent = "You need to provide a suitable input";
 
     } else if (element.validity.tooShort) {
-        error.textContent = `The ${element.name} input must be at least ${element.minlength}
+        error.textContent = `The ${element.name} input must be at least ${element.minLength}
         characters, and you have entered ${element.value.length}`;
 
     } else if (element.validity.tooLong) {
-        error.textContent = `The ${element.name} input may not exceed ${element.minlength}
+        error.textContent = `The ${element.name} input may not exceed ${element.minLength}
         characters, and you have entered ${element.value.length}`;
     }
 
 };
+
+//here is the generic event listener
+
+const inputListener = (element) => {
+    let error = errors[`${element.id}`];
+
+    //if everything is good, then we set the errors to nothing
+    if (element.validity.valid && element.value !== "") {
+        error.textContent = "";
+        element.setCustomValidity("");
+    }
+    else {
+        if (element.value !== "") {
+            element.setCustomValidity("");
+        }
+        //if we have an problem or it's empty, show errors
+        showError(element);
+    }
+};
+
+email.addEventListener("input", () => {
+    inputListener(email);
+})
+
+country.addEventListener("input", () => {
+    inputListener(country);
+})
+
+postalCode.addEventListener("input", () => {
+    inputListener(postalCode);
+})
+
+pw.addEventListener("input", () => {
+    inputListener(pw);
+})
+
+pwConfirm.addEventListener("input", () => {
+    inputListener(pwConfirm);
+})
